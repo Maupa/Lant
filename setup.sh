22724ff43d5695efb154806908827020c01d7ebb
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2014-10-22 23:45:23 +1300

    Created setup file

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+#remove juju
+sudo apt-get -y remove juju*
+
+#updating and instaling depencies
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install git vim
+
+#installing XAMPP
+wget "http://downloads.sourceforge.net/project/xampp/XAMPP%20Linux/1.8.3/xampp-linux-x64-1.8.3-5-installer.run" -O xampp.run
+chmod +x "xampp.run"
+sudo ./xampp.run --mode unattended
+
+sudo cat <<EOT >> /etc/init.d/vagrant_startup
+#!/usr/bin/env bash
+sudo /opt/lampp/lampp start
+EOT
+sudo chmod +x /etc/init.d/vagrant_startup
+sudo update-rc.d vagrant_startup defaults
+
+sudo /opt/lampp/lampp start
