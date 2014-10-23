e9ac21639ded64df1a375e5dbaae3463c48bba11
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2014-10-23 17:06:09 +1300

    Moved to custom build LAMP stack

--- setup.sh
+++ setup.sh
@@ -3,34 +3,38 @@
 #Updating and instaling depencies
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get -y install git vim
 
-sudo cat /dev/null > /etc/update-motd.d/98-cloudguest
+#Pre-set deb-configs
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
 
-#Installing XAMPP
-wget "http://downloads.sourceforge.net/project/xampp/XAMPP%20Linux/1.8.3/xampp-linux-x64-1.8.3-5-installer.run" -O xampp.run
-chmod +x "xampp.run"
-sudo ./xampp.run --mode unattended
+sudo debconf-set-selections <<< 'phpmyadmin phpmyadmin/dbconfig-install boolean true'
+sudo debconf-set-selections <<< 'phpmyadmin phpmyadmin/app-password-confirm password root'
+sudo debconf-set-selections <<< 'phpmyadmin phpmyadmin/mysql/admin-pass password root'
+sudo debconf-set-selections <<< 'phpmyadmin phpmyadmin/mysql/app-pass password root'
+sudo debconf-set-selections <<< 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2'
 
-#Ownership fix for htdocs
-sudo chown -R vagrant:vagrant /opt/lampp/htdocs/
+#Installing LAMP stack
+sudo apt-get -y install apache2
+sudo apt-get -y install mysql-server libapache2-mod-auth-mysql php5-mysql
+sudo apt-get -y install php5 libapache2-mod-php5 php5-mcrypt
 
-#Security config fix for XAMPP
-sudo wget "https://gist.githubusercontent.com/herrniemand/364490075d2f54eb8bfe/raw/e3f90120c5f973d7021efa61c6110a29590be9a9/httpd-xampp.conf" -O /opt/lampp/etc/extra/httpd-xampp.conf
+#MySQL configuration
+sudo mysql_install_db
+sudo apt-get -y install phpmyadmin
 
-#Add startup script
-sudo cat <<EOT >> /etc/init.d/vagrant_startup
-#!/usr/bin/env bash
-sudo /opt/lampp/lampp start
+#PHP configuration
+sudo cat <<EOT > /etc/apache2/mods-enabled/dir.conf
+<IfModule mod_dir.c>
+        DirectoryIndex index.php index.html index.cgi index.pl index.php index.xhtml index.htm
+</IfModule>
 EOT
-sudo chmod +x /etc/init.d/reboot_setup
-sudo update-rc.d reboot_setup defaults
 
-#Message to reboot.
-echo "################################"
-echo "#                              #"
-echo "#  You VM requires reboot      #"
-echo "#         Please run           #"
-echo "#  vagrant halt && vagrant up  #"
-echo "#                              #"
-echo "################################"
+#Ownership fix for html
+sudo chown -R vagrant:vagrant /var/www/html
+
+#Creates index.php if it is not exists
+if [ ! -f /var/www/html/index.php ] ; then echo "<?php phpinfo(); ?>" > /var/www/html/index.php; fi
+
+#Apache restart
+sudo /etc/init.d/apache2 restart
